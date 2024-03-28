5540209b81a33ff65784506aecf221ce1527bea3
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-28 01:47:25 -0400

    comment out

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //in JS, every element is a node but not every node is an element
 
 //getElementById()
-const historyList = document.getElementById("history-list");
+// const historyList = document.getElementById("history-list");
 // const historyListItems = historyList.getElementById('item');
-historyList.innerHTML = "<li>History Item</li>";
+// historyList.innerHTML = "<li>History Item</li>";
 
 //querySelector()
