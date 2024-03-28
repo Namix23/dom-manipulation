51a42242fe4415e189494a508b1ed5e9b2cc5dfe
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-28 01:08:37 -0400

    create variable inside historyList

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //in JS, every element is a node but not every node is an element
 
 //getElementById()
-const historyList = document.getElementById
+const historyList = document.getElementById('history-list');
+const historyListItems = historyList.getElementById('item');
+
 //querySelector()
