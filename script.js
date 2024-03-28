512869a32266d59bf467ba27f050086047e8a4c0
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-28 02:26:54 -0400

    add value to number input

--- script.js
+++ script.js
@@ -6,5 +6,11 @@
 // historyList.innerHTML = "<li>History Item</li>";
 
 //querySelector()
-const headingNode = document.querySelector("h1");
-headingNode.innerHTML = "new heading";
+// const headingNode = document.querySelector("h1");
+// headingNode.innerHTML = "new heading";
+
+const historyNode = document.getElementById("history-list");
+
+const inputNode = document.querySelector("#guess-input");
+
+inputNode.value = "20";
