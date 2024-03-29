38007ceae0513431d699eec89ca999aa6dc6318e
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-29 00:36:47 -0400

    add template literal to alert message if number is correct

--- script.js
+++ script.js
@@ -9,38 +9,41 @@
 // const headingNode = document.querySelector("h1");
 // headingNode.innerHTML = "new heading";
 
-const historyNode = document.getElementById("history-list");
 
-const inputNode = document.querySelector("#guess-input");
 
 // inputNode.value = "20";
- 
-//generates random number btwn 0 and 1
-const correctNumber = Math.floor(Math.random() *100);
-inputNode.value = correctNumber;
 
+//generates random number btwn 0 and 1
+
+const historyNode = document.getElementById("history-list");
+const inputNode = document.querySelector("#guess-input");
+const correctNumber = Math.floor(Math.random() * 100);
 function checkAnswer(){
+  const guessInput = inputNode.value;
+
+  //store the result to be printed in guest history
+  let resultText = "";
 
   //get the guess input & check against the correct answer
   //tell the user the resullt
-  //store the result to be printed in guest history
-  const guessInput = inputNode.value;
   
   //create a variable to store the result
-  let resultText = "";
   
   //if guess user is blank, ask user for input & return
-  if (guessInput == ''){
-    alert ('enter a number!');
+  if (guessInput == ""){
+    alert ("enter a number!");
     return;
   }
   if (guessInput > correctNumber) {
-    alert ('guess lower');
-    resultText = 'lower';
+    alert ("guess lower");
+    resultText = "lower";
   }
   else if (guessInput < correctNumber){
-    alert('guess higher');
-    resultText = 'higher';
+    alert("guess higher");
+    resultText = "higher";
   }
-
+  else {
+    alert (`correct! The answer was ${correctNumber}`);
+    resultText = "correct";
+  }
 }
